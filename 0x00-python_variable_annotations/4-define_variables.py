0a36dba5e3db5ec34af0b1673da6aa4e9e960579
Anotibills <anotibills@gmail.com>
2024-01-11 21:51:59 +0100

    Task 4 correction

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -8,11 +8,3 @@ a = 1
 pi = 3.14
 i_understand_annotations = True
 school = "Holberton"
-
-
-if __name__ == '__main__':
-    print(f"a is a {type(a)} with a value of {a}")
-    print(f"pi is a {type(pi)} with a value of {pi}")
-    print(f"i_understand_annotations is a {type_info} "
-          f"with a value of {i_understand_annotations}")
-    print(f"school is a {type(school)} with a value of {school}")
